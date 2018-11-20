5ae3c98a4844c2ba2cd447c0a0a2521f98c4b889
machida-mn <machida_mn@complex.ist.hokudai.ac.jp>
2018-11-20 19:15:25 +0900

    Add test case

--- test/test.cpp
+++ test/test.cpp
@@ -54,7 +54,7 @@ TEST(fnmatch, translate) {
   EXPECT_EQ(cppglob::translate("file[1-9].txt"), "file[1-9]\\.txt");
   EXPECT_EQ(cppglob::translate("file[!1-9].txt"), "file[^1-9]\\.txt");
   EXPECT_EQ(cppglob::translate("file[^1-9\\].txt"), "file[\\1-9\\\\]\\.txt");
-  EXPECT_EQ(cppglob::translate("[-+{}()?[$.a"), R"(\[\-\+\{\}\(\).\[\$\.a)");
+  EXPECT_EQ(cppglob::translate("[]-+{}()?$.a"), R"(\[\]\-\+\{\}\(\).\$\.a)");
 }
 
 TEST(fnmatch, filter) {
@@ -69,40 +69,68 @@ TEST(fnmatch, filter) {
 }
 
 TEST(glob, iglob) {
+  test_in_dir _;
+
+  ASSERT_TRUE(fs::create_directories("apple"));
+  ASSERT_TRUE(fs::create_directories("banana"));
+  create_file("apple/apple.txt");
+  create_file("banana/apple.txt");
+
   cppglob::glob_iterator it = cppglob::iglob("./*"), end;
   ASSERT_NE(it, end);
+  ASSERT_EQ(end, cppglob::iglob());
 
   std::vector<fs::path> vec(it, end);
-  EXPECT_GT(vec.size(), 0L);
+  unorderd_compare_results(vec, {"./apple", "./banana"});
 
-  for (auto&& elem : vec) {
-    ASSERT_FALSE(elem.empty());
-  }
+  cppglob::glob_iterator it2 = cppglob::iglob("**", true);
+  vec = std::vector<fs::path>(it2, end);
+
+  unorderd_compare_results(vec, {"apple", "banana", "apple/apple.txt",
+                                 "banana/apple.txt"});
 }
 
 TEST(glob, glob) {
   test_in_dir _;
 
-  ASSERT_TRUE(fs::create_directories("a/b/c"));
+  ASSERT_TRUE(fs::create_directories("a/b/c/.d"));
+  ASSERT_TRUE(fs::create_directories("e"));
 
   // create file
-  create_file("d.txt");
-  create_file("a/e.txt");
-  create_file("a/f.txt");
+  create_file("f.txt");
+  create_file("a/g.txt");
+  create_file("a/h.txt");
+  create_file("e/g.txt");
 
-  std::vector<fs::path> vec = cppglob::glob("a/e.txt");
-  unorderd_compare_results(vec, {"a/e.txt"});
+  std::vector<fs::path> vec = cppglob::glob("a/g.txt");
+  unorderd_compare_results(vec, {"a/g.txt"});
+
+  vec = cppglob::glob("a/b/");
+  unorderd_compare_results(vec, {"a/b/"});
+
+  vec = cppglob::glob("a/b/c/.*");
+  unorderd_compare_results(vec, {});
+
+  vec = cppglob::glob("j.txt");
+  unorderd_compare_results(vec, {});
 
   vec = cppglob::glob("././///./*");
-  std::vector<fs::path> corrects = {"./././a", "./././d.txt"};
+  std::vector<fs::path> corrects = {"./././a", "./././f.txt", "./././e"};
   unorderd_compare_results(vec, corrects);
 
   vec = cppglob::glob("./a/../a/b/../*/");
   unorderd_compare_results(vec, {"./a/../a/b/../b/"});
 
   vec = cppglob::glob("a/?.*");
-  corrects = {"a/e.txt", "a/f.txt"};
+  corrects = {"a/g.txt", "a/h.txt"};
   unorderd_compare_results(vec, corrects);
+
+  vec = cppglob::glob("*/g.txt");
+  corrects = {"a/g.txt", "e/g.txt"};
+  unorderd_compare_results(vec, corrects);
+
+  vec = cppglob::glob("*/i.txt");
+  unorderd_compare_results(vec, {});
 }
 
 TEST(glob, recursive) {
@@ -118,6 +146,11 @@ TEST(glob, recursive) {
 
   std::vector<fs::path> vec = cppglob::glob("a/**/*.txt", true);
   std::vector<fs::path> corrects = {"a/e.txt", "a/f.txt", "a/b/g.txt"};
+
+  unorderd_compare_results(vec, corrects);
+
+  vec = cppglob::glob("**", true);
+  corrects = {"a", "a/b", "a/b/c", "d.txt", "a/e.txt", "a/f.txt", "a/b/g.txt"};
 
   unorderd_compare_results(vec, corrects);
 }
