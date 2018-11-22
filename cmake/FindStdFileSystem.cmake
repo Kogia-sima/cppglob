069fecf412ac52e63e26b504961a25f11c7b91e9
machida-mn <machida_mn@complex.ist.hokudai.ac.jp>
2018-11-23 08:38:24 +0900

    Support Clang on Mac OSX

--- cmake/FindStdFileSystem.cmake
+++ cmake/FindStdFileSystem.cmake
@@ -1,6 +1,8 @@
 if(MSVC)
   # avoid erro on find_package_handle_standard_args
   set(STDFILESYSTEM_LIBRARY "__")
+elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+  set(STDFILESYSTEM_LIBRARY c++fs)
 else()
   set(STDFILESYSTEM_LIBRARY stdc++fs)
 endif()
@@ -8,7 +10,7 @@ endif()
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(STDFILESYSTEM DEFAULT_MSG STDFILESYSTEM_LIBRARY)
 
-if (MSVC)
+if (STDFILESYSTEM_LIBRARY STREQUAL "__")
   set(STDFILESYSTEM_LIBRARY "")
 endif()
 
